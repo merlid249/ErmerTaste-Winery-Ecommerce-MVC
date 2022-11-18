ac9e89a2b3748e6e1b2efbf32b254476b244c7ff
Merlind Mahmuti <merlind249@gmail.com>
2022-11-18 20:29:48 +0100

    Add project files.

---
+++ ErmerTaste/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ErmerTaste.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
