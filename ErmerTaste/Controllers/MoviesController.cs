5ef6300e75cdefdd752a0ff054d956aa8d2b1ce6
Merlind Mahmuti <merlind249@gmail.com>
2022-11-23 12:48:19 +0100

    x

--- ErmerTaste/Controllers/MoviesController.cs
+++ ErmerTaste/Controllers/MoviesController.cs
@@ -1,25 +1,22 @@
-﻿
-using ErmerTaste.Data;
+﻿using ErmerTaste.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace ErmerTaste.Controllers
 {
-    public class MoviesControllers : Controller
+    public class MoviesController : Controller
     {
-
         private readonly AppDbContext _contect;
-        public MoviesControllers(AppDbContext contect)
+        public MoviesController(AppDbContext contect)
         {
             _contect = contect;
         }
         public async Task<IActionResult> Index()
+        
         {
-            var allMovies = await _contect.Movies.ToListAsync();
+            var allMovies = await _contect.Movies.Include(n=>n.Cinema).OrderBy(n=>n.Name).ToListAsync();
 
             return View(allMovies);
         }
-
     }
 }
-
