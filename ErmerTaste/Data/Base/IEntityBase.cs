63da49a90a4ff8acfaeaf1538c7ff8d2f8936643
Merlind Mahmuti <merlind249@gmail.com>
2022-12-10 22:39:35 +0100

    Entity Base

---
+++ ErmerTaste/Data/Base/IEntityBase.cs
@@ -0,0 +1,7 @@
+﻿namespace ErmerTaste.Data.Base
+{
+    public interface IEntityBase
+    {
+        int Id { get; set; }
+    }
+}
---
+++ ErmerTaste/Data/Base/IEntityBaseRespository.cs
@@ -0,0 +1,14 @@
+﻿using ErmerTaste.Models;
+
+namespace ErmerTaste.Data.Base
+{
+    public interface IEntityBaseRespository<T> where T : class,IEntityBase, new()
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task<T> UpdateAsync(int id, T entity);
+        Task DeleteAsync(int id);
+
+    }
+}
