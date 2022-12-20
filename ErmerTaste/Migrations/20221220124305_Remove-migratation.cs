c1cd89f33231005d6c86e4f9e6b704dd027a0066
Merlind Mahmuti <merlind249@gmail.com>
2022-12-20 14:01:11 +0100

    New

---
+++ ErmerTaste/Migrations/20221220124305_Remove-migratation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ErmerTaste.Migrations
+{
+    /// <inheritdoc />
+    public partial class Removemigratation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
